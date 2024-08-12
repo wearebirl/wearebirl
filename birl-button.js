a2f7ca65a4e25a9d5a27a7870fdee2f5c68218b2
edday57 <edward57day@gmail.com>
2024-08-12 13:11:52 +0100

    Update birl-button.js

--- birl-button.js
+++ birl-button.js
@@ -2,17 +2,17 @@
     (function() {
         document.addEventListener("DOMContentLoaded", function() {
             console.log('Birl Button is ready!');
-            const customHTML = `
-                <div class="custom-button">
-                    <button>Click Me!</button>
-                </div>
-            `;
-            const birlButtons = document.querySelectorAll('birlbutton');
-            birlButtons.forEach(function(birlButton) {
-                const newElement = document.createElement('div');
-                newElement.innerHTML = customHTML;
-                birlButton.replaceWith(newElement.firstChild);
-            });
+            // const customHTML = `
+            //     <div class="custom-button">
+            //         <button>Click Me!</button>
+            //     </div>
+            // `;
+            // const birlButtons = document.querySelectorAll('birlbutton');
+            // birlButtons.forEach(function(birlButton) {
+            //     const newElement = document.createElement('div');
+            //     newElement.innerHTML = customHTML;
+            //     birlButton.replaceWith(newElement.firstChild);
+            // });
         })
     })();
 </script>
