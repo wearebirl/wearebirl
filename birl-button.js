80281741471803654657dad8abfa031338ca7be8
edday57 <edward57day@gmail.com>
2024-08-12 13:36:15 +0100

    js button

--- birl-button.js
+++ birl-button.js
@@ -1,18 +1,18 @@
-<script>
-    (function() {
-        document.addEventListener("DOMContentLoaded", function() {
-            console.log('Birl Button is ready!');
-            // const customHTML = `
-            //     <div class="custom-button">
-            //         <button>Click Me!</button>
-            //     </div>
-            // `;
-            // const birlButtons = document.querySelectorAll('birlbutton');
-            // birlButtons.forEach(function(birlButton) {
-            //     const newElement = document.createElement('div');
-            //     newElement.innerHTML = customHTML;
-            //     birlButton.replaceWith(newElement.firstChild);
-            // });
-        })
-    })();
-</script>
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Page has been loaded");
+  
+    const customHTML = `
+      <div class="custom-button">
+        <button>Click Me!</button>
+      </div>
+    `;
+  
+    const birlButtons = document.querySelectorAll(".birlbutton"); // Select by class
+  
+    birlButtons.forEach(function (birlButton) {
+      const newElement = document.createElement("div");
+      newElement.innerHTML = customHTML;
+      birlButton.replaceWith(newElement); // Replace directly with newElement
+    });
+  });
+  
