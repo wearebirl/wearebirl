cb77016313ca7a180ca910d24a060dffb3991691
edday57 <edward57day@gmail.com>
2024-08-12 13:04:45 +0100

    Addedd button js

---
+++ birl-button.js
@@ -0,0 +1,17 @@
+<script>
+    (function() {
+        document.addEventListener("DOMContentLoaded", function() {
+            const customHTML = `
+                <div class="custom-button">
+                    <button>Click Me!</button>
+                </div>
+            `;
+            const birlButtons = document.querySelectorAll('birlbutton');
+            birlButtons.forEach(function(birlButton) {
+                const newElement = document.createElement('div');
+                newElement.innerHTML = customHTML;
+                birlButton.replaceWith(newElement.firstChild);
+            });
+        });
+    })();
+</script>
