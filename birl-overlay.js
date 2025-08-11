73a6ba6f572735dd0d6e2cfb0daa291b12e702de
Theo Newell <theonewell@hotmail.co.uk>
2025-08-11 13:49:26 +0100

    added overlay basic fetch

--- birl-overlay.js
+++ birl-overlay.js
@@ -4,19 +4,30 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
     }
 
-    const customerId = overlayElement.dataset.customerId || "Guest";
+    const customerId = overlayElement.dataset.customerId;
+    if(!customerId) {
+        return;
+    }
 
-    overlayElement.innerHTML = `
-    <div style="
-      position: fixed;
-      bottom: 20px;
-      right: 20px;
-      background-color: #333;
-      color: #fff;
-      padding: 10px 15px;
-      border-radius: 8px;
-    ">
-      Customer ID: ${customerId}
-    </div>
-  `;
+    (async () => {
+        const response = await fetch(`https://2d5795b16aa8.ngrok-free.app/public/v1/880015a6-9949-45d4-a941-240d3639c604/customers/${customerId}`, {headers: {'ngrok-skip-browser-warning': '12345'}});
+        const customerOrders = await response.json();
+
+        const itemHtml = customerOrders.map((item) => {
+            return `<div>
+                        <img style="width: 80px; height: auto;" src="${item.image_url}" />
+                        <p>${item.max_credit}</p>
+                    </div>`
+        })
+
+        overlayElement.innerHTML = `
+            <div class="birl-overlay-container">
+              Save up to £100 
+              <div style="display: flex; flex-direction: row;">
+                   ${itemHtml.join("\n")}
+</div>
+        
+            </div>
+          `;
+    })()
 });
