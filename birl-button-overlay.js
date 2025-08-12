f4f2b451a1f1eaf68becc9c4d2aa5b645279219a
Theo Newell <theonewell@hotmail.co.uk>
2025-08-12 13:41:02 +0100

    testing height adjustment for button display

--- birl-button-overlay.js
+++ birl-button-overlay.js
@@ -23,14 +23,17 @@ document.addEventListener("DOMContentLoaded", () => {
         // // TODO need to pull off the company naming and button titles to insert into the button layout
 
         buttonElement.innerHTML = `
+        <div class="birl-button-overlay-container-master">
         <div class="birl-button-overlay-container">
-            <div class="birl-button-overlay-logo">
+                   <div class="birl-button-overlay-logo">
                 <img class="birl-button-overlay-icon" src='https://storage.googleapis.com/birl-public/logos/birl-purple-icon.svg' />
             </div>
             <div>
                 <h1 class="birl-button-overlay-title">Get money off this item today</h1>
                 <h2 class="birl-button-overlay-subtitle">Trade-in FRAHM jackets you no longer use</h2>
             </div>
+</div>
+ 
         </div>
         `;
 
