32dfc9ff328a96560189d502bb4eb21443bd93be
Theo Newell <theonewell@hotmail.co.uk>
2025-08-12 13:56:06 +0100

    formatted the overlay

--- birl-button-overlay.js
+++ birl-button-overlay.js
@@ -9,33 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
     }
 
-    (async () => {
-        // const response = await fetch(
-        //     `https://api.wearebirl.com/public/v1/${storeId}`,
-        //     { method: "GET" }
-        // );
-        //
-        // if(!response.ok) {
-        //     return;
-        // }
-        //
-        // const organisation = await response.json();
-        // // TODO need to pull off the company naming and button titles to insert into the button layout
-
-        buttonElement.innerHTML = `
-        <div class="birl-button-overlay-container-master">
+    buttonElement.innerHTML = `
         <div class="birl-button-overlay-container">
-                   <div class="birl-button-overlay-logo">
-                <img class="birl-button-overlay-icon" src='https://storage.googleapis.com/birl-public/logos/birl-purple-icon.svg' />
+            <div class="birl-button-overlay-content">
+                <div class="birl-button-overlay-logo">
+                    <img class="birl-button-overlay-icon" src='https://storage.googleapis.com/birl-public/logos/birl-purple-icon.svg' />
+                </div>
+                <div>
+                    <h1 class="birl-button-overlay-title">Get money off this item today</h1>
+                    <h2 class="birl-button-overlay-subtitle">Trade-in FRAHM jackets you no longer use</h2>
+                </div>
             </div>
-            <div>
-                <h1 class="birl-button-overlay-title">Get money off this item today</h1>
-                <h2 class="birl-button-overlay-subtitle">Trade-in FRAHM jackets you no longer use</h2>
-            </div>
-</div>
- 
         </div>
         `;
-
-    })()
 });
